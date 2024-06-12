13c623332f5a17c727fd74270936bb0d26072178
muhamedhaamdy <mmhamdyyy@gmail.com>
2024-06-12 16:43:05 +0300

    Tasks

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python3
-'''crateing task'''
+""" The basics of async """
 
-from asyncio import Task, create_task
+import asyncio
+from typing import List
 
-wait_random = __import__('0-basic_async_syntax').wait_random
+task_wait_random = __import__('3-tasks').task_wait_random
 
 
-def task_wait_random(max_delay: int) -> Task:
-    """tasking"""
-    return create_task(wait_random(max_delay))
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    spawn task_wait_random n times with the specified max_delay.
+    """
+    tasks = [task_wait_random(max_delay) for _ in range(n)]
+    return [await task for task in asyncio.as_completed(tasks)]
