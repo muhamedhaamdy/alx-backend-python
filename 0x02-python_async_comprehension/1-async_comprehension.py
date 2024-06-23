c7a716d2cf8503eecce4e94cc31619deecba9dcd
muhamedhaamdy <mmhamdyyy@gmail.com>
2024-06-24 02:32:01 +0300

    Async Generator

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""
+    This module contains a coroutine called `async_comprehension`
+    that returns a list of random numbers
+"""
+import asyncio
+
+async_generator = __import__('0-async_generator').async_generator
